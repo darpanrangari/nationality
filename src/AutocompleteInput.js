db73ea5fa6b6a822c4cc635b50d0cdf3b9767e5c
Darpan Rangari <darpanrangari@192.168.1.104>
2023-05-11 16:57:08 +0100

    updated events

--- src/AutocompleteInput.js
+++ src/AutocompleteInput.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const AutocompleteInput = ({ options }) => {
   const [inputValue, setInputValue] = useState('');
   const [filteredOptions, setFilteredOptions] = useState([]);
+  const [showOptions, setShowOptions] = useState(false);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (inputRef.current && !inputRef.current.contains(event.target)) {
+      setShowOptions(false);
+    }
+  };
 
   const handleInputChange = (event) => {
     const value = event.target.value;
@@ -13,25 +28,39 @@ const AutocompleteInput = ({ options }) => {
       option.toLowerCase().includes(value.toLowerCase())
     );
     setFilteredOptions(filtered);
+    setShowOptions(true);
   };
 
   const handleOptionClick = (value) => {
     setInputValue(value);
     setFilteredOptions([]);
+    setShowOptions(false);
+  };
+
+  const handleInputClick = (value) => {
+    setFilteredOptions(options);
+    setShowOptions(true);
   };
 
   return (
-    <div>
+    <div className="autocomplete-container">
       <input
         type="text"
         value={inputValue}
         onChange={handleInputChange}
+        onClick={handleInputClick}
         placeholder="Type here..."
+        className="autocomplete-input"
+        ref={inputRef}
       />
-      {filteredOptions.length > 0 && (
-        <ul>
+      {showOptions && filteredOptions.length > 0 && (
+        <ul className="autocomplete-dropdown">
           {filteredOptions.map((option, index) => (
-            <li key={index} onClick={() => handleOptionClick(option)}>
+            <li
+              key={index}
+              onClick={() => handleOptionClick(option)}
+              className="autocomplete-option"
+            >
               {option}
             </li>
           ))}
